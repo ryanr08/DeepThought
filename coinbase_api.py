5f9e9613e5aedcdcce24b025b7929e5edebc082a
Ryan Riahi <ryanr08@gmail.com>
2021-05-23 12:18:26 -0700

    Updates

--- coinbase_api.py
+++ coinbase_api.py
@@ -1,4 +1,4 @@
-import json, hmac, hashlib, time, base64, requests
+import json, hmac, hashlib, time, base64, requests, sys, pytz
 from requests.auth import AuthBase
 from datetime import datetime, timedelta
 
@@ -36,6 +36,9 @@ try:
     f.close()
 except:
     print("Please set up your API key in key.txt.\nSet your key.txt file as:\nAPI_KEY\nAPI_SECRET\nAPI_PASS")
+    sys.exit(1)
+
+timezone = pytz.timezone('US/Pacific')
 
 # set up authentication with API key
 api_url = 'https://api.pro.coinbase.com/'
@@ -44,17 +47,33 @@ auth = CoinbaseExchangeAuth(API_KEY, API_SECRET,  API_PASS)
 # Calculate a simple moving average for a given coin over n_days
 def calculateSMA(coin_id, n_days):
     # get historical data
-    today = datetime.now().isoformat()
-    n_days_ago = (datetime.now() - timedelta(days=n_days)).isoformat()
+    granularity = 0
+    if (n_days >= 150):
+        granularity = 86400
+    elif (n_days >= 12):
+        granularity = 21600
+    elif (n_days > 3):
+        granularity = 3600
+    elif (n_days > 1):
+        granularity = 900
+    elif (n_days > 0.2):
+        granularity = 300
+    else:
+        granularity = 60
 
+    # get ISO time of right now and time of n_days ago
+    today = datetime.now(tz=timezone).isoformat()
+    n_days_ago = (datetime.now(tz=timezone) - timedelta(hours=24*n_days)).isoformat()
+
+    # set up parameters for API
     params = {
         'start': str(n_days_ago),
         'end': str(today),
-        'granularity': 300 #86400
+        'granularity': granularity
     }
 
     res = requests.get(api_url + f'products/{coin_id}/candles' + "?start=" + str(params['start']) + "&end=" + str(params['end']) + "&granularity=" + str(params['granularity']), auth=auth)
-    #print(res.json())
+    
     # calculate sma based off close values
     sum = 0
     count = 0
@@ -67,11 +86,9 @@ def calculateSMA(coin_id, n_days):
 
 # Get the current market price of a specific coin
 def getCurrentPrice(coin_id):
-    # getting 24hr stats
     try:
-        r = requests.get(api_url + f'products/{coin_id}/stats', auth=auth).json()['last']
+        r = requests.get(api_url + f'products/{coin_id}/ticker', auth=auth).json()['price']
         return float(r)
-
     except:
         print("ERROR getting values from API")
         exit(1)
@@ -82,7 +99,7 @@ def getAccountInfo():
     r = requests.get(api_url + 'accounts', auth=auth)
     return r.json()
 
-#Place an order
+# Place a buy order
 def Buy(coin_id):   
     order = {
         'size': 1.0,
@@ -92,6 +109,32 @@ def Buy(coin_id):
     }
     r = requests.post(api_url + 'orders', json=order, auth=auth)
     return r.json()
+
+# Place a sell order
+def Sell(coin_id):
+    order = {
+        'size': 1.0,
+        'price': 1.0,
+        'side': 'sell',
+        'product_id': coin_id,
+    }
+    r = requests.post(api_url + 'orders', json=order, auth=auth)
+    return r.json()  
+
+# get the price from num_mins ago
+def getPreviousPrice(coin_id, num_mins):
+    
+    n_mins_ago = (datetime.now(tz=timezone) - timedelta(minutes=num_mins)).isoformat()
+
+    # set up parameters for API
+    params = {
+        'start': str(n_mins_ago),
+        'end': str(datetime.now(tz=timezone).isoformat()),
+        'granularity': 60
+    }
+
+    res = requests.get(api_url + f'products/{coin_id}/candles' + "?start=" + str(params['start']) + "&end=" + str(params['end']) + "&granularity=" + str(params['granularity']), auth=auth)
+    return (res.json()[-1][3])
 
 # getting single product data
 # r = requests.get(api_url + 'products/BTC-USD', auth=auth)
--- cryptoBot.py
+++ cryptoBot.py
@@ -33,24 +33,47 @@ def sell(currentPrice, balance, coins_held):
     writelog(f"balance is at ${balance}\n")
     return balance, coins_held
 
-counterrr = 0
-currPrice = 0
-BALANCE = 100000
-COINS_HELD = 0
+def main():
 
-# Daemon
-while (counterrr < 7200):
-    time.sleep(5)
-    counterrr += 1
-    currPrice = cbase.getCurrentPrice(ticker)
-    sma200 = cbase.calculateSMA(ticker, 1)#calculateSMA(ticker, 200)
-    if (currPrice < (sma200)):
-        if (BALANCE >= currPrice):
-            BALANCE, COINS_HELD = purchase(currPrice, BALANCE, COINS_HELD)
-    elif (currPrice > (sma200)):
-        if (COINS_HELD > 0):
-            BALANCE, COINS_HELD = sell(currPrice, BALANCE, COINS_HELD)
+    counter = 0
+    currPrice = 0
+    sleep_time = 5 # number of seconds to wait each iteration
+    BALANCE = 100000 # Amount in dollars of available paper money
+    COINS_HELD = 0
 
-BALANCE, COINS_HELD = sell(currPrice, BALANCE, COINS_HELD)
-writelog(str(datetime.now()))
-writelog(f"FINAL BALANCE: {BALANCE} + '\n")
+    bought_at = 0  # last price we bought at
+    buy = False
+    sell = False
+    monitor = False
+
+    # Daemon
+    while (counter < 17280):
+        time.sleep(sleep_time)
+        counter += 1
+        currPrice = cbase.getCurrentPrice(ticker)
+        sma = cbase.calculateSMA(ticker, 1)
+
+        if (currPrice <= (0.95) * sma):
+            buy = True
+
+        if (bought_at != 0 and (currPrice > (1.01) * bought_at or currPrice <= (0.98) * bought_at)):
+            sell = True
+
+        if (buy):
+            if (BALANCE >= currPrice):
+                BALANCE, COINS_HELD = purchase(currPrice, BALANCE, COINS_HELD)
+                bought_at = currPrice
+            buy = False
+        elif (sell):
+            if (COINS_HELD > 0):
+                BALANCE, COINS_HELD = sell(currPrice, BALANCE, COINS_HELD)
+                bought_at = 0
+            sell = False
+
+
+    BALANCE, COINS_HELD = sell(currPrice, BALANCE, COINS_HELD)
+    writelog(str(datetime.now()))
+    writelog(f"FINAL BALANCE: {BALANCE} + '\n")
+
+if __name__ == "__main__":
+    main()
